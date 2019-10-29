ce95b224cc2a0ac5882f39f9b0fd4dabf5614d05
Rubikoid <dimazotoff8@gmail.com>
2019-10-29 19:42:45 +0300

    Allocating page fixed

--- kernel/src/utils/memory.c
+++ kernel/src/utils/memory.c
@@ -5,26 +5,29 @@
 
 #include <kernel/utils/memory.h>
 
-uint32_t last_page_ID;
+size_t last_page_ID;
 volatile uint32_t *page_directory_ptr;
 volatile uint32_t *page_table_ptr;
 
 
 void init_memory_manager() {
     last_page_ID = VIRT_BASE + KERNEL_TABLE_COUNT * TABLE_SIZE + PAGE_SIZE;  // additional page for why not
-    page_directory_ptr = &boot_page_directory;
-    page_table_ptr = &boot_page_table;
+    page_directory_ptr = (volatile uint32_t*) &boot_page_directory;
+    page_table_ptr = (volatile uint32_t*) &boot_page_table;
 }
 
 void *alloc_page() {
     DEBUG_ASM;
-    //uint32_t PDEi = (last_page_ID) >> 22;
-    //uint32_t PTEi = ((last_page_ID & TABLE_BIT_FIELD) >> 12);
-    uint32_t x = page_directory_ptr[800];
+    uint32_t PDEi = (last_page_ID) >> 22;
+    uint32_t PTEi = ((last_page_ID & TABLE_BIT_FIELD) >> 12);
+    uint32_t PDE = page_directory_ptr[PDEi];
+    page_directory_ptr[PDEi] |= 0x003;
     DEBUG_ASM;
-    //boot_page_table[((PDE & PDTE_BIT_FIELD)) + PTEi] |= 0x003;
+    ((uint32_t *)((size_t) VIRT_BASE + (PDE & PDTE_BIT_FIELD)))[PTEi] |= 0x003;
     DEBUG_ASM;
+    return 0;
 }
 
 void free_page() {
+
 }
